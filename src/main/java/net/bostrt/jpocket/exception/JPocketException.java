da7c3e57a15735534268a6df7bb34192294e9403
Robert Bost <bostrt@gmail.com>
2012-06-09 12:38:17 -0400

    Added signup support.

---
+++ src/main/java/net/bostrt/jpocket/exception/JPocketException.java
@@ -0,0 +1,11 @@
+package net.bostrt.jpocket.exception;
+
+public class JPocketException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public JPocketException(String message) {
+		super(message);
+	}
+
+}
